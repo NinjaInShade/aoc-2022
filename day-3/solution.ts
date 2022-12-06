a3f1830856cddac76b7634b183d5c34adf7582ec
Leon <leonmichalak6@gmail.com>
2022-12-06 21:53:14 +0000

    submission: solutions for day 3 part 1/2

--- day-3/solution.ts
+++ day-3/solution.ts
@@ -1,16 +1,71 @@
-import type { ParserFunction } from '../parseFile';
+type ChallengeInput = string[];
 
-const part1 = (input: string[]) => {
-  console.log(input);
-  //
+const itemPriorities = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+const part1 = (input: ChallengeInput) => {
+  let sum = 0;
+
+  for (const bag of input) {
+    const commonType = calculateItemType(bag);
+
+    if (commonType) {
+      sum += calculateSum(commonType);
+    }
+  }
+
+  return sum;
 };
 
-const part2 = (input: string[]) => {
-  //
+const part2 = (input: ChallengeInput) => {
+  let sum = 0;
+
+  let groupBags: string[] = [];
+
+  for (let i = 0; i < input.length + 1; i++) {
+    const bag = input[i];
+
+    // Every 3 bags is one group, need to do calculations on each group
+    if (i !== 0 && i % 3 === 0) {
+      const firstBagLetters = groupBags[0].split('');
+      const secondBagLetters = groupBags[1].split('');
+      const thirdBagLetters = groupBags[2].split('');
+
+      for (const letter of firstBagLetters) {
+        if (secondBagLetters.includes(letter) && thirdBagLetters.includes(letter)) {
+          sum += calculateSum(letter);
+          break;
+        }
+      }
+
+      groupBags = [];
+    }
+
+    groupBags.push(bag);
+  }
+
+  return sum;
 };
 
-const parser: ParserFunction = (result, newlineSplit) => {
-  return result
+const calculateItemType = (bag: string): string | undefined => {
+  const [compartmentOne, compartmentTwo] = [bag.slice(0, bag.length / 2), bag.slice(bag.length / 2)];
+  for (let i = 0; i < compartmentOne.length; i++) {
+    if (compartmentTwo.split('').includes(compartmentOne[i])) {
+      return compartmentOne[i];
+    }
+  }
+};
+
+const calculateSum = (commonType: string): number => {
+  const priorityIndex = itemPriorities.findIndex((priority) => priority == commonType?.toLowerCase());
+  if (commonType === commonType?.toLowerCase()) {
+    return priorityIndex + 1;
+  } else {
+    return priorityIndex + 27;
+  }
+};
+
+const parser = (input: string, newlineSplit: string): ChallengeInput => {
+  return input
     .trimEnd()
     .split(`${newlineSplit}${newlineSplit}`)
     .map((groupedInput) => groupedInput.split(newlineSplit))[0];
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import parseFile from './parseFile';
 import day1Solution from './day-1/solution';
 import day2Solution from './day-2/solution';
+import day3Solution from './day-3/solution';
 
 const solutionImports = {
   1: day1Solution,
   2: day2Solution,
+  3: day3Solution,
 };
 
 const main = async () => {
